d3129409839617b5127bfd66f6d235f4c5dd9145
SENAC <joanes.senac@gmail.com>
2025-11-19 12:08:39 +0100

    Ajout initial

---
+++ Pingouin.java
@@ -0,0 +1,33 @@
+
+/**
+ * Décrivez votre classe Pingouin ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Pingouin
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private int x;
+
+    /**
+     * Constructeur d'objets de classe Pingouin
+     */
+    public Pingouin()
+    {
+        // initialisation des variables d'instance
+        x = 0;
+    }
+
+    /**
+     * Un exemple de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y   le paramètre de la méthode
+     * @return     la somme de x et de y
+     */
+    public int sampleMethod(int y)
+    {
+        // Insérez votre code ici
+        return x + y;
+    }
+}
